2abd9772063d5b615fb7c9b96d35a177f4a84164
mrdulin <novaline.dulin@gmail.com>
2026-09-17 17:14:15 +0800

    docs: remove commented-out shareStrategy from Module Federation configuration

--- complete-react-case/component-app/rspack.config.js
+++ complete-react-case/component-app/rspack.config.js
@@ -48,7 +48,6 @@ module.exports = {
     new ModuleFederationPlugin({
       experiments: { asyncStartup: true },
       name: 'component_app',
-      // shareStrategy: 'loaded-first',
       filename: 'remoteEntry.js',
       exposes: {
         './Button': './src/Button.jsx',
